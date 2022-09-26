ccb61b8f9c4cc711cd0d06c131253ea0509a991a
Natalia <nataliaanicharicoinformatica7a@gmail.com>
2022-09-26 14:23:42 -0500

    Mi proyecto

---
+++ src/App.vue
@@ -0,0 +1,49 @@
+<script>
+import ComponenteTarea from './components/ComponenteTarea.vue';
+export default {
+  components: {
+    ComponenteTarea
+  },
+ //data() {
+   // return {
+     // tareaNueva: "",
+      //tareaPendiente: "",
+      //tareaFinalizada: ""
+    //}
+ // }
+}
+</script>
+
+<template>
+
+  <div class="wrapper" id="root">
+    <div class="box">
+      <ComponenteTarea></ComponenteTarea>
+      <!--<ComponenteTarea :llamar="valores"></ComponenteTarea>-->
+    </div>
+  </div>
+
+</template>
+
+<style scoped>
+.wrapper {
+  display: grid;
+  justify-content: center;
+  align-items: center;
+  height: 50%;
+  margin-top: 280px;
+  width: 100%;
+
+}
+
+.box {
+  background-color: rgb(211, 210, 206);
+  width: 800px;
+  height: 380px;
+  justify-content: center;
+  align-items: center;
+  display: grid;
+  border-radius: 10px;
+  box-shadow: 10px 10px 15px 2px rgba(97, 92, 92, 0.671);
+}
+</style>
---
+++ src/components/ComponenteTarea.vue
@@ -0,0 +1,76 @@
+<template lang="">
+    <div>
+    <div class="tareauno">
+<h3>Nueva Tarea</h3>
+<input type="text" class="nueva" v-model="tareaNueva"> <input type="button" v-on:click="btnAgregar" value="agregar">
+
+    </div>
+   
+    <div class="tareados">
+        <h3>Tarea Pendiente</h3>
+<input type="text" id="tareaP" class="cajaTareaPendiente" :value="tareaPendiente"> <input type="button" v-on:click="btnListo" class="btnListo" value="listo"> <input type="button" class="btnEliminar" v-on:click="btnEliminar" value="Eliminar">
+
+    </div>
+    <div class="tareatres">
+        <h3>Tarea Finalizada</h3>
+<!--<input type="text" class="cajaTareaFinalizada" :value="tareaFinalizada"> -->
+        <select name="seleccionar" id="slcTareaFinalizada" :value="tareaFinalizada" ></select>
+    </div>
+    </div>
+</template>
+<script>
+export default {
+  // props: ['llamar'],
+    data() {
+        return {
+            tareaNueva: "",
+            tareaPendiente:"",
+            tareaFinalizada:""
+        }
+    },
+    methods: {
+        btnAgregar(){
+        const dateobj = new Date()
+            let fecha = dateobj.toUTCString()
+            this.tareaPendiente=(this.tareaNueva +"fecha["+ fecha +"]-")
+            this.tareaNueva = ""
+        },
+        btnListo(){
+            const $sele = document.getElementById('slcTareaFinalizada')
+            const option = document.createElement('option');
+            let tareaFinalizada=(this.tareaPendiente)
+            option.value=tareaFinalizada;
+            option.text=tareaFinalizada;
+            $sele.appendChild(option);
+            const opcionCambiar=()=>{
+                console.log("Cambio");
+            }
+            $sele.addEventListener("change",opcionCambiar);
+            this.tareaPendiente= ""
+        },
+        btnEliminar(){
+            document.getElementById("tareaP").value=""
+        },
+    },
+}
+</script>
+<style>
+.tareados,
+.tareatres,
+.tareauno {
+    margin-top: 20px;
+    margin-bottom: 10px;
+    width: 690px;
+
+}
+
+.nueva,
+.cajaTareaPendiente,
+.cajaTareaFinalizada {
+    width: 80%;
+}
+
+#slcTareaFinalizada{
+    width: 555px;
+}
+</style>
